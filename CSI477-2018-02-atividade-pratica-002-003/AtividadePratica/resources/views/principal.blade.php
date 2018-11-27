52fb8fbca58b6e0962333b306086a97b926ef6fe
GiovanniFraga <giovanni.fraga@aluno.ufop.edu.br>
2018-11-27 15:10:36 -0200

    a

--- CSI477-2018-02-atividade-pratica-002-003/AtividadePratica/resources/views/principal.blade.php
+++ CSI477-2018-02-atividade-pratica-002-003/AtividadePratica/resources/views/principal.blade.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,8 +7,10 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
 </head>
 <body>
+
 
   <nav class="navbar navbar-inverse">
     <div class="container-fluid">
@@ -15,7 +18,7 @@
         <a class="navbar-brand" href="#">PetShop</a>
       </div>
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Produtos</a></li>
+        <li class="active"><a href="/">Produtos</a></li>
 
         <form class="navbar-form navbar-left" action="/action_page.php">
           <div class="form-group">
@@ -23,16 +26,16 @@
           </div>
           <button type="submit" class="btn btn-default">Buscar</button>
         </form>
-        <li class="active" ><a href="#">Carrinho</a></li>
-        <li class="active"><a href="#">Minhas Compras</a></li>
+        <li class="active" ><a href="carrinho">Carrinho</a></li>
+        <li class="active"><a href="compras">Minhas Compras</a></li>
       </ul>
 
 
 
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-home"></span> Administrativo</a></li>
+        <li><a href="login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        <li><a href="register"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="login"><span class="glyphicon glyphicon-home"></span> Administrativo</a></li>
       </ul>
 
     </div>
@@ -40,10 +43,35 @@
   </nav>
 
 
+  @section('content')
+  <div class="container">
+      <div class="row justify-content-center">
+          <div class="col-md-8">
+              <div class="card">
+                  <div class="card-header">Dashboard</div>
+
+                  <div class="card-body">
+                      @if (session('status'))
+                          <div class="alert alert-success" role="alert">
+                              {{ session('status') }}
+                          </div>
+                      @endif
+
+                      You are logged in!
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+  @endsection
+
 
   <section class="content">
     @yield('conteudo')
+
   </section>
+
+
 
   <section class="author">
     @yield('autor')
--- CSI477-2018-02-atividade-pratica-002-003/AtividadePratica/routes/web.php
+++ CSI477-2018-02-atividade-pratica-002-003/AtividadePratica/routes/web.php
@@ -14,3 +14,29 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('cliente', function () {
+    return view('cliente');
+});
+
+Route::get('carrinho', function () {
+    return view('carrinho');
+});
+
+Route::get('login', function () {
+    return view('login');
+});
+
+Route::get('register', function () {
+    return view('register');
+});
+
+Route::get('compras', function () {
+    return view('compras');
+});
+Route::get('adiministrador', function () {
+    return view('adiministrador');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
